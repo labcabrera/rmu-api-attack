435fc74d8d85128905c5584a089ecad9f4c26413
labcabrera <lab.cabrera@gmail.com>
2025-08-06 00:38:11 +0200

    chore: bump version to 0.1.0

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,5 @@ RMU API Attack - REST API for attack management
 This package contains all the REST API logic for managing attack information in the RMU system.
 """
 
-__version__ = "1.0.0"
+__version__ = "0.1.0"
 __author__ = "lab.cabrera@gmail.com"
