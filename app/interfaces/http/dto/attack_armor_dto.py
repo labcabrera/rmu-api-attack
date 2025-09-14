5d6a24ea1ca9934d19bf635788723d724b9b2b20
labcabrera <lab.cabrera@gmail.com>
2025-09-14 13:51:52 +0200

    Fix dto model.

--- app/interfaces/http/dto/attack_armor_dto.py
+++ app/interfaces/http/dto/attack_armor_dto.py
@@ -7,28 +7,28 @@ class AttackArmorDTO(BaseModel):
     """DTO for attack armor"""
 
     at: int | None = Field(default=None, description="Overall armor type")
-    body_at: int | None = Field(default=None, description="Body armor type")
-    head_at: int | None = Field(default=None, description="Head armor type")
-    arms_at: int | None = Field(default=None, description="Arms armor type")
-    legs_at: int | None = Field(default=None, description="Legs armor type")
+    bodyAt: int | None = Field(default=None, description="Body armor type")
+    headAt: int | None = Field(default=None, description="Head armor type")
+    armsAt: int | None = Field(default=None, description="Arms armor type")
+    legsAt: int | None = Field(default=None, description="Legs armor type")
 
     model_config = ConfigDict(json_schema_extra={"example": {"at": 1}})
 
     def to_entity(self):
         return AttackArmor(
             at=self.at,
-            body_at=self.body_at,
-            head_at=self.head_at,
-            arms_at=self.arms_at,
-            legs_at=self.legs_at,
+            body_at=self.bodyAt,
+            head_at=self.headAt,
+            arms_at=self.armsAt,
+            legs_at=self.legsAt,
         )
 
     @classmethod
     def from_entity(cls, entity: AttackArmor) -> "AttackArmorDTO":
         return cls(
             at=entity.at,
-            body_at=entity.body_at,
-            head_at=entity.head_at,
-            arms_at=entity.arms_at,
-            legs_at=entity.legs_at,
+            bodyAt=entity.body_at,
+            headAt=entity.head_at,
+            armsAt=entity.arms_at,
+            legsAt=entity.legs_at,
         )
