e430b7b18a536f430e65ccc5ff3303483dbc8d4f
labcabrera <lab.cabrera@gmail.com>
2025-09-24 23:26:00 +0200

    Fix local docker run variables.

--- docker-run.sh
+++ docker-run.sh
@@ -10,6 +10,7 @@ docker build -t labcabrera/rmu-api-attack:latest .
 
 docker run -d -p 8000:8000 --network rmu-network --name rmu-api-attack -h rmu-api-attack \
   -e RMU_MONGO_ATTACK_URI='mongodb://admin:admin@rmu-mongo:27017/rmu-attack?authSource=admin' \
+  -e RMU_API_ATTACK_TABLES_URL='http://rmu-api-attack-tables:3005/v1' \
   -e PORT='8000' \
   labcabrera/rmu-api-attack:latest
 
